05a896d16cda9448ce57f3a33c77090357961836
HarryAhnHS <hongsuhahn@gmail.com>
2023-11-05 22:01:56 +0800

    include pen and BG color inputs and implement color changes in JS while maintaining previous grid colors

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function createGrid(n) {
             gridUnit.style['background-color'] = 'black';
         });
     }
-}
+};
 
-function resetGrid(n) {
+function resetGrid(n, pColor, bgColor) {
     // Remove all divs in parent 
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
@@ -29,21 +29,48 @@ function resetGrid(n) {
         gridUnit.style.width = `${100/n}%`;
         gridUnit.style.height = `${100/n}%`;
         gridUnit.style.flex = 'auto';
-        gridUnit.style['background-color'] = 'white';
+        gridUnit.style['background-color'] = bgColor;
 
         grid.appendChild(gridUnit);
 
         gridUnit.addEventListener('mouseover', () => {
-            gridUnit.style['background-color'] = 'black';
+            gridUnit.style['background-color'] = pColor;
         });
     }
-}
+};
 
-// Create n x n grid of divs within .sketchbook
+function changePenColor(pColor) {
+    const gridList = grid.children;
+    for (let i = 0; i < gridList.length; i++) {
+        gridList[i].addEventListener('mouseover', () => {
+            gridList[i].style['background-color'] = pColor;
+        });
+    }
+};
+
+function changeBGColor(bgColor) {
+    const gridList = grid.children;
+    for (let i = 0; i < gridList.length; i++) {
+        gridList[i].addEventListener('mouseover', () => {
+            gridList[i].style['background-color'] = pColor;
+        });
+    }
+};
+
+// MAIN EXEC:
+// Create n x n grid of divs 
 const grid = document.querySelector('.grid');
 const defaultSize = 16;
 
 createGrid(defaultSize);
+
+// Default colors: 
+const defaultPenColor = '#000000';
+const defaultBGColor = '#FFFFFF';
+
+// Color inputs
+const penColorInput = document.querySelector('#pen-color');
+const bgColorInput = document.querySelector('#bg-color');
 
 // Grid size range input
 const range = document.querySelector('#num-grid');
@@ -54,27 +81,32 @@ range.value = defaultSize;
 rangeFeedback.textContent = `${defaultSize} x ${defaultSize}`;
 
 var inputSize = range.value;
-// update range feedback for each tick of range input
+
+// Update range feedback for each tick of range input
 range.addEventListener('input', (e) => {
     rangeFeedback.textContent = `${e.target.value} x ${e.target.value}`;
     inputSize = e.target.value;
 });
 
-// apply button for grid size range input
+// Apply button for grid size range input - Keep colors and background
 const rangeApply = document.querySelector('#grid-apply');
 rangeApply.addEventListener('click', () => {
-    resetGrid(inputSize);
+    resetGrid(inputSize, penColorInput.value, bgColorInput.value);
 });
 
-// reset button
+// Reset button - reset colors, backgrounds, grid size to default
 const reset = document.querySelector('#reset-grid');
 reset.addEventListener('click', () => {
-    resetGrid(defaultSize);
+    resetGrid(defaultSize, defaultPenColor, defaultBGColor);
     range.value = defaultSize;
     rangeFeedback.textContent = `${defaultSize} x ${defaultSize}`;
 
+    penColorInput.value = '#000000';
+    bgColorInput.value = '#FFFFFF';
 })
 
-
+penColorInput.addEventListener('input', (e) => {
+    changePenColor(`${e.target.value}`);
+});
 
 
